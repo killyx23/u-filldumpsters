1bb98908988ddae83352867176ee1a5973da3c8e
erikras1223 <razmataz1223@hotmail.com>
2025-08-19 16:26:05 -0600

    second commit

--- src/lib/customSupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://REDACTED_PROJECT_REF.supabase.co';
-const supabaseAnonKey = 'REDACTED_SUPABASE_ANON_JWT';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
