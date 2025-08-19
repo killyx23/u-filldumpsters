bed6dd3f58ec88751eaf7f7024075d855a53c627
erikras1223 <razmataz1223@hotmail.com>
2025-08-19 16:26:05 -0600

    second commit

--- src/pages/CustomerDetailPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { CustomerDetailView } from '@/components/admin/customer-detail/CustomerDetailView';
-
-const CustomerDetailPage = () => {
-    return <CustomerDetailView />;
-};
-
-export default CustomerDetailPage;
