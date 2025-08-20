46b3152dbc4d75502d1b3f2aaa88996e1e18c620
erikras1223 <razmataz1223@hotmail.com>
2025-08-20 11:54:33 -0600

    delete zone identifier

--- src/components/admin/DetailItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const DetailItem = ({ label, value }) => (
-    <div className="flex justify-between">
-        <p className="font-semibold text-blue-200">{label}:</p>
-        <p className="text-right">{value}</p>
-    </div>
-);
