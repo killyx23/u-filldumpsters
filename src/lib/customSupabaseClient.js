e1875ddb384702fd721e7978e15442af160a6bd9
erikras1223 <razmataz1223@hotmail.com>
2026-01-31 20:54:08 -0700

    semi functiional can get through booking

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://REDACTED_PROJECT_REF.supabase.co';
 const supabaseAnonKey = 'REDACTED_SUPABASE_ANON_JWT';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+export default customSupabaseClient;
+
+export { 
+    customSupabaseClient,
+    customSupabaseClient as supabase,
+};
