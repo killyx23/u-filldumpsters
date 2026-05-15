6d400352f393fca47a904a6fcd6b96d165ace54f
brandbekras <brandon@u-filldumpsters.com>
2026-05-14 20:39:07 -0600

    update roles to make them idempontent

--- supabase/roles.sql
+++ supabase/roles.sql
@@ -1,2 +1,10 @@
+-- supabase/roles.sql
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'booking_creator') THEN
 CREATE ROLE "booking_creator";
+END IF;
+END
+$$;
+
 GRANT "booking_creator" TO "postgres";
